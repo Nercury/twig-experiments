532da18aeed576fb3341352230106415a8a6a80b
Nerijus Arlauskas <nercury@gmail.com>
2015-09-22 19:43:29 +0300

    Better AST printing.

--- ast-hello-world/run.php
+++ ast-hello-world/run.php
@@ -5,8 +5,15 @@ require __DIR__.'/../vendor/autoload.php';
 $env = new Twig_Environment();
 $lexer = new Twig_Lexer($env);
 
-$tokens = $lexer->tokenize("Hello {{ value }}");
+$tokens = $lexer->tokenize("Hello {{ aaa }} {% block right %} {% endblock %}");
 $module = $env->parse($tokens);
 
-var_dump($module);
+$class = new ReflectionClass(Twig_Node::class);
 
+$attr_field = $class->getProperty("attributes");
+$attr_field->setAccessible(true);
+
+$nodes_field = $class->getProperty("nodes");
+$nodes_field->setAccessible(true);
+
+echo $module->__toString();
